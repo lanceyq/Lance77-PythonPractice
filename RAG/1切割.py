a183a17caf73e0b5606f28b35cf2153a64540aa4
lanceyq <1982376970@qq.com>
2025-09-14 16:07:23 +0800

    删除不必要的文件

--- RAG/1切割.py
+++
@@ -1,8 +0,0 @@
-# 1.读取文件内容
-with open("E:/laboratory/code/trea-setup/Lance77-PythonPractice/RAG/knowledge/中医v1.txt", encoding='utf-8', mode='r') as fp:
-    data = fp.read()
-
-# 2.根据换行切割
-chunk_list = data.split("\n\n")
-chunk_list = [chunk for chunk in chunk_list if chunk]
-print(chunk_list)
